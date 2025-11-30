bc902d5e7ea3c375af97c9402be07003779580ef
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2025-11-30 21:14:07 +0100

    Add logs for 1b

--- deliverables/1b/Torch-MLIR_CIRCT.sh
+++ deliverables/1b/Torch-MLIR_CIRCT.sh
@@ -1,2 +1,3 @@
 nix shell "github:NixOS/nixpkgs/ee09932cedcef15aaf476f9343d1dea2cb77e261#llvmPackages_21.mlir" -c ./my-demo.sh
 
+yosys -m ~/yosys-slang/build/slang.so -p "read_slang /home/roland/hot-chips-2022-pytorch-circt-hls-demo/my-dot.sv"
